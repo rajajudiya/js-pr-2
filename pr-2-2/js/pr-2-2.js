00a57168f13cc036f598453195e81e42f3c251c0
rajajudiya <153293660+rajajudiya@users.noreply.github.com>
2024-05-15 11:23:01 +0530

    Add files via upload

---
+++ pr-2-2/js/pr-2-2.js
@@ -0,0 +1,27 @@
+var month = 12;
+switch (month) {
+
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        console.log('winter')
+        break;
+
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+        console.log('Summer')
+        break;
+
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+        console.log('Monsoon')
+        break;
+    default:
+        console.log('your choice is wrong')
+        break;
+}
